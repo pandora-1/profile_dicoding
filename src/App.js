db0d774b361ec744f76b32f1bd5a06fd2cd1a7c5
pandora-1 <michaelkahari.mk@gmail.com>
2023-10-05 00:36:09 +0700

    add html and styling

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="wrapper">
+        <div class="img-area">
+          <div class="inner-area">
+            <img src="https://storage.googleapis.com/profile_michael/FotoDiri.jpg" alt="" />
+          </div>
+        </div>
+        <div class="name">Marcellus Michael Herman Kahari</div>
+        <div class="about">Interested in Software Engineer and have internship experiences in Pegipegi, Garena, eFishery, and BFI Finance.</div>
+        <div class="buttons">
+          <a href="https://www.linkedin.com/in/marcellus-michael-herman-kahari/"><button>Linkedin</button></a>
+          <a href="https://drive.google.com/file/d/1Rh2MrmPipwwZ5QAHgpY8uWOhJ9dNf7A1/view?usp=sharing"><button>CV</button></a>
+          <a href="https://github.com/pandora-1"><button>Github</button></a>
+        </div>
+      </div>
     </div>
   );
 }
